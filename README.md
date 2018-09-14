6e2481e33b6a0288fe99ea6548f01008fa20b22f
Tristan Kernan <tristankernan@Tristans-MBP.fios-router.home>
2018-09-14 13:27:59 -0400

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Is the Bathroom Available?
+
+For the truly lazy, or otherwise inconvenienced.
+
+## Motivation
+
+There is only one bathroom in my apartment. Due to the layout, it is impossible to predict the occupancy of the restroom without approaching it. This is annoying and demands a hi-tech solution!
+
+## How
+
+A spare Android phone running the [Is the Bathroom Available? app](). This app is programmed to watch for changes to the ambient light and send updates to this backend server.
+
+When the ambient light is high (based on some pre-defined threshold), the bathroom is occupied and unavailable. Otherwise, the bathroom is probably unoccupied and probably available.
+
+## Setup
+
+1. Set a good password in `app.config`.
+2. Build the application: `$ docker-compose build`
+3. Let mysql perform its first-run initialization: `$ docker-compose db up`
+4. When it's done, about a minute later, kill it with `Control-c`
+5. Run the full application: `$ docker-compose up`
+
+Use the typical `docker-compose` commands to run in the background.
+
+## Changelog
+
+### V0.1
+
+- Display current bathroom availability.
+
+## License
+
+GPL-3.0
---
+++ app.py
@@ -0,0 +1,54 @@
+from datetime import datetime
+from flask import Flask, render_template, request
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:supersecurepassword@db/log'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# thanks to https://stackoverflow.com/a/35819857
+app.config['SQLALCHEMY_POOL_SIZE'] = 100
+app.config['SQLALCHEMY_POOL_RECYCLE'] = 280
+
+app.config.from_envvar('BATHROOM_AVAILABILITY_CONF')
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class AvailabilityRecord(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    available = db.Column(db.Boolean, nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+
+@app.route("/")
+def index():
+    # thanks to https://stackoverflow.com/a/8551979
+    record = AvailabilityRecord.query.order_by('-id').first()
+    available = record.available
+    
+    print("[#index] Available: ", available)
+    
+    return render_template("main.html", available=available)
+
+@app.route("/update", methods=['POST'])
+def update():
+    data = request.get_json()
+    available = data['available']
+    password = data['password']
+
+    if password != app.config['SUPER_SECRET_AND_SECURE_PASSWORD']:
+        return 'Invalid Password', 403
+
+    if isinstance(available, bool):
+        record = AvailabilityRecord(available=available)
+        db.session.add(record)
+        db.session.commit()        
+
+    print("[#update] Available: ", available)
+
+    return 'OK', 200
