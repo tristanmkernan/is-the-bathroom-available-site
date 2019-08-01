a73f284bcbd048cfd2150ddad29c32161741a25f
Tristan K <tristanmkernan@gmail.com>
2019-07-31 20:12:02 -0400

    feat: upgrade deps, reformat with black, remove pipenv, improve dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
-FROM tiangolo/uwsgi-nginx-flask:python3.6-alpine3.7
+FROM tiangolo/uwsgi-nginx-flask:python3.6-alpine3.8
 
-COPY . /app
+COPY requirements.txt /app/requirements.txt
 
 RUN pip install -r /app/requirements.txt
+
+COPY . /app
--- app.py
+++ app.py
@@ -6,10 +6,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////app/app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-app.config.from_envvar('BATHROOM_AVAILABILITY_CONF')
+app.config.from_envvar("BATHROOM_AVAILABILITY_CONF")
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
@@ -24,27 +24,28 @@ class AvailabilityRecord(db.Model):
 @app.route("/")
 def index():
     # thanks to https://stackoverflow.com/a/8551979
-    record = AvailabilityRecord.query.order_by('-id').first()
+    record = AvailabilityRecord.query.order_by("-id").first()
     available = record.available
-    
+
     print("[#index] Available: ", available)
-    
+
     return render_template("main.html", available=available)
 
-@app.route("/update", methods=['POST'])
+
+@app.route("/update", methods=["POST"])
 def update():
     data = request.get_json()
-    available = data['available']
-    password = data['password']
+    available = data["available"]
+    password = data["password"]
 
-    if password != app.config['SUPER_SECRET_AND_SECURE_PASSWORD']:
-        return 'Invalid Password', 403
+    if password != app.config["SUPER_SECRET_AND_SECURE_PASSWORD"]:
+        return "Invalid Password", 403
 
     if isinstance(available, bool):
         record = AvailabilityRecord(available=available)
         db.session.add(record)
-        db.session.commit()        
+        db.session.commit()
 
     print("[#update] Available: ", available)
 
-    return 'OK', 200
+    return "OK", 200
